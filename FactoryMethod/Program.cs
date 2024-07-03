dddc107043b0d22dc7b98cb9a07e3b95202bdcd7
Re9Wine <asckortan@gmail.com>
2024-07-03 15:20:53 +0300

    Added practice code

---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,22 @@
+﻿using FactoryMethod.Example;
+
+Console.WriteLine("Выберите тип запускаемого проекта:\n" +
+                  "1. Учебный пример\n" +
+                  "2. Реальный пример\n" +
+                  "Любой другой символ. Завершить работу программы");
+
+var projectType = Console.ReadKey().KeyChar;
+
+Console.WriteLine();
+
+switch (projectType)
+{
+    case '1':
+        new Client().Main();
+        break;
+    case '2':
+        Console.WriteLine("Я хз как тут может быть практическая реализация :)");
+        break;
+}
+
+Console.WriteLine("Завершение работы программы");      
