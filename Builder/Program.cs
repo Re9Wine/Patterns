714f0c2dcc39dee6578731b7ace3acf2adb2e68b
Re9Wine <asckortan@gmail.com>
2024-07-04 18:07:28 +0300

    Create Programm

---
+++ Builder/Program.cs
@@ -0,0 +1,37 @@
+﻿using Builder;
+using Builder.Builders;
+
+var director = new Director();
+var bmwBuilder = new BMWBuilder();
+var toyotaBuilder = new ToyotaBuilder();
+
+Console.WriteLine("Создание машины без Строителя:");
+director.BuildFullFeaturedCar();
+
+director.SetBuilder = bmwBuilder;
+            
+Console.WriteLine("Создание машины BMW с базовой комплектацией:");
+director.BuildMinimalViableCar();
+Console.WriteLine(bmwBuilder.GetCar());
+
+Console.WriteLine("Создание машины BMW с полной комплектацией:");
+director.BuildFullFeaturedCar();
+Console.WriteLine(bmwBuilder.GetCar());
+
+director.SetBuilder = toyotaBuilder;
+
+Console.WriteLine("Создание машины Toyota с базовой комплектацией:");
+director.BuildMinimalViableCar();
+Console.WriteLine(toyotaBuilder.GetCar());
+
+Console.WriteLine("Создание машины Toyota с полной комплектацией:");
+director.BuildFullFeaturedCar();
+Console.WriteLine(toyotaBuilder.GetCar());
+
+// Помните, что паттерн Строитель можно использовать без класса
+// Директор.
+Console.WriteLine("Произвольная комплектация BMW:");
+bmwBuilder.Reset();
+bmwBuilder.BuildModel();
+bmwBuilder.BuildEngine();
+Console.Write(bmwBuilder.GetCar());
